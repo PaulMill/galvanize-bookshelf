792804b4b6957dacd227a66dd67a07dea88dfc6f
PaulMill <usa.paul@icloud.com>
2017-01-04 14:40:26 -0800

    create migrate file books

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,18 @@
 'use strict';
 
 module.exports = {
-  development: {},
+  development: {
+    client: 'pg',
+    connection: 'postgress://localhost/bookshelf_dev'
+  },
 
-  test: {},
+  test: {
+    client: 'pg',
+    connection: 'postgress://localhost/bookshelf_test'
+  },
 
-  production: {}
+  production: {
+    client: 'pg',
+    connection: 'postgress://localhost/bookshelf_prod'
+  }
 };
