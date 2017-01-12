7cef495ca5d1de5f53501450f970db679f6bd3f2
PaulMill <usa.paul@icloud.com>
2017-01-11 19:54:22 -0800

    Prepare the Heroku1

--- migrations/20170110172822_favorites.js
+++ migrations/20170110172822_favorites.js
@@ -4,15 +4,15 @@ exports.up = function(knex) {
   return knex.schema.createTable('favorites', (table) => {
     table.increments();
     table.integer('book_id')
-      .notNullable()
       .references('books.id')
       .inTable('books')
+      .notNullable()
       .onDelete('CASCADE')
       .index();
     table.integer('user_id')
-      .notNullable()
       .references('users.id')
       .inTable('users')
+      .notNullable()
       .onDelete('CASCADE')
       .index();
     table.timestamps(true, true);
